08ab0b71c3645216df5754c60e3446f517af2dca
loganmoon03 <loganmun03@gmail.com>
2026-09-01 15:25:11 -0400

    added my file

--- project1/DochanMoon.java
+++ project1/DochanMoon.java
@@ -1,4 +1,4 @@
-package CS2114Group29.project1.project1;
+package CS2114Group29.project1;
 public class DochanMoon {
     public static void main(String[] args) {
         System.out.println("Your Name is on the team.");
