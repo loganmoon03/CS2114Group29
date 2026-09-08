e4c5e072b361301d3708ca536c2e6e93d0629680
suhanac2026S <suhanac@vt.edu>
2026-09-08 14:57:31 -0400

    Update Suhana.java

--- project1/Suhana.java
+++ project1/Suhana.java
@@ -1,5 +1,5 @@
 public class YourName {
     public static void main(String[] args) {
-        System.out.println("Your Name is on the team.");
+        System.out.println("Suhana is on the team.");
     }
 }
