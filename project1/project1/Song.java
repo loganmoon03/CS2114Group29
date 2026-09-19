1da44516b7713c9cd29345c2b03d91f9f10ca36e
Miguel Nueno Armisén <miguel33@vt.edu>
2026-09-19 19:42:11 -0400

    Song class and test
    Mostly done need to finish some things

---
+++ project1/project1/Song.java
@@ -0,0 +1,100 @@
+package project1;
+
+// -------------------------------------------------------------------------
+/**
+ *  Represents a song.
+ *  Describes the parameters and fields that the song class uses
+ *  and sets up getters for them, also has an in playlist method
+ *  that returns true or false depending on if it finds the song.
+ * 
+ *  @author miguel33
+ *  @version 19 Sept 2026
+ */
+public class Song {
+    
+    //~ Fields ................................................................
+    private String name;
+    private String artist;
+    private String duration;
+    private int playCount;
+    private String genre;
+    
+    //~ Constructors ..........................................................
+    
+    // ----------------------------------------------------------
+    /**
+     * Create a new Song object.
+     * @param name
+     * @param artist
+     * @param duration
+     * @param playCount
+     * @param genre
+     */
+    public Song(String name, String artist, String duration,
+                int playCount, String genre) {
+        this.name = name;
+        this.artist = artist;
+        this.duration = duration;
+        this.playCount = playCount;
+        this.genre = genre;
+    }
+    
+    //~Public  Methods ........................................................
+    
+    // ----------------------------------------------------------
+    /**
+     * Gets the song's name.
+     * @return name of song.
+     */
+    public String getName() {
+        return name;
+    }
+    
+    // ----------------------------------------------------------
+    /**
+     * Gets the name of the song's artist.
+     * @return artist.
+     */
+    public String getArtist() {
+        return artist;
+    }
+    
+    // ----------------------------------------------------------
+    /**
+     * Gets the song's duration in the format x:xx (minutes:seconds).
+     * @return duration of song.
+     */
+    public String getDuration() {
+        return duration;
+    }
+    
+    // ----------------------------------------------------------
+    /**
+     * Gets the number of times the song has been played.
+     * @return playcount.
+     */
+    public int getPlayCount() {
+        return playCount;
+    }
+    
+    // ----------------------------------------------------------
+    /**
+     * Gets the song's genre.
+     * @return genre.
+     */
+    public String getGenre() {
+        return genre;
+    }
+    
+    // ----------------------------------------------------------
+    /**
+     * Determines if the song is in the playlist.
+     * @return true if in playlist, false if not.
+     */
+    public boolean inPlaylist(Playlist playlist) {
+        if (playlist == null) {
+            return false;
+        }
+        return playlist.search(this);
+    }
+}
---
+++ project1/project1/SongTest.java
@@ -0,0 +1,52 @@
+package project1;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+// -------------------------------------------------------------------------
+/**
+ *  Represents the tests for the song class.
+ *  Tests the five getters for the name, artist, duration,
+ *  playcount and genre as well as the in playlist method.
+ * 
+ *  @author miguel33
+ *  @version 19 Sept 2026
+ */
+class SongTest {
+
+    //~ Fields ................................................................
+    private Song song1;
+
+    //~ Constructors ..........................................................
+    
+    // ----------------------------------------------------------
+    /**
+     * Set up method that runs at the beginning of every test.
+     */
+    public void setUp() {
+        song1 = new Song("Enter Sandman", "Metallica", "5:30", 3, "Rock");
+    }
+    
+    
+    //~Public  Methods ........................................................
+    
+    // ----------------------------------------------------------
+    /**
+     * Tests the five getters in song.
+     */
+    public void testGetters() {
+        assertEquals("Enter Sandman", song1.getName());
+        assertEquals("Metallica", song1.getArtist());
+        assertEquals("5:30", song1.getDuration());
+        assertEquals(3, song1.getPlayCount());
+        assertEquals("Rock", song1.getGenre());
+    }
+    
+    // ----------------------------------------------------------
+    /**
+     * Tests inPlaylist().
+     */
+    public void testInPlaylist() {
+        //
+    }
+    
+}
